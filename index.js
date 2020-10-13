182ba69390a4f2417cd86d2d989322b685805956
esy <yenigun13@gmail.com>
2020-10-13 18:47:53 -0400

    Api: index config with midware

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-if (process.env.NODE_ENV !== "production") require("../secrets");
+// pull in api keys as needed
+// if (process.env.NODE_ENV !== "production") require("../secrets");
+
+// logware
+app.use(morgan("dev"));
+
+// body parse mdwre
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use("/auth", require("./auth"));
+// app.use("/api", require("./api"));
+
+app.get("/", (req, res) => {
+  res.send("Nice");
+});
+
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+const startServer = () => {
+  app.listen(PORT);
+  console.log(`tracking stacks on ${PORT}`);
+};
+
+startServer();
