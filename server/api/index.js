37357921e81aa378771c7097d3c8eeec62f053f5
esy <yenigun13@gmail.com>
2020-10-26 17:05:52 -0400

    API: add routing modules

--- server/api/index.js
+++ server/api/index.js
@@ -1,4 +1,12 @@
+"use strict";
+
 const router = require("express").Router();
 module.exports = router;
 
 router.use("/users", require("./routes/users"));
+router.use("/teams", require("./routes/teams"));
+router.use("/organizations", require("./routes/organizations"));
+router.use("/issues", require("./routes/issues"));
+router.use("/projects", require("./routes/projects"));
+router.use("/messages", require("./routes/messages"));
+router.use("/tags", require("./routes/tags"));
