3e6c3a0ba42dc0cd52538a1116056afb20b7aa7e
esyeng <yenigun13@gmail.com>
2020-11-06 18:50:18 -0500

    API: Users routes

--- server/api/routes/users.js
+++ server/api/routes/users.js
@@ -5,13 +5,51 @@ module.exports = users;
 // all users (test)
 users.get("/", async (req, res, next) => {
   try {
-    const users = await User.findAll({
-      include: {
-        model: Team,
-      },
-    });
+    const users = await User.findAll();
     res.json(users);
   } catch (err) {
     next(err);
   }
 });
+
+users.get("/:userId", async (req, res, next) => {
+  try {
+    const { userId } = req.params;
+    const cur = await User.findByPk(userId);
+    res.json(cur);
+  } catch (err) {
+    next(err);
+  }
+})
+
+users.post("/", async (req, res, next) => {
+  try {
+    // const { first, last, user, title, bio, profileImageUrl, email, password } = req.body;
+    const newUser = await User.create(req.body)
+    res.send(newUser);
+  } catch (err) {
+    next(err);
+  }
+})
+
+users.put("/:userId", async (req, res, next) => {
+  try {
+    const { userId } = req.params;
+    const cur = await User.findByPk(userId);
+    await cur.update(req.body);
+    res.json(cur);
+  } catch (err) {
+    next(err);
+  }
+})
+
+users.delete("/:userId", async (req, res, next) => {
+  try {
+    const { userId } = req.params;
+    const cur = await User.findByPk(userId);
+    await User.delete(cur);
+    res.sendStatus(204);
+  } catch (err) {
+    next(err);
+  }
+})
