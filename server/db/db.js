91c86cf86f4111f127662f36537cd9fc64345d35
esy <yenigun13@gmail.com>
2020-12-06 20:51:46 -0500

    commit db changes

--- server/db/db.js
+++ server/db/db.js
@@ -1,20 +1,14 @@
 const Sequelize = require("sequelize");
-const dbConfig = require("./db.config");
 
 const databaseName =
   "stacktrack" + process.env.NODE_ENV === "test" ? "test" : "";
 
-const db = new Sequelize(dbConfig.db, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  logging: true,
-  dialect: dbConfig.dialect,
-  pool: {
-    max: dbConfig.pool.max,
-    min: dbConfig.pool.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle,
-  },
-});
-// console.log(db); || `postgres://localhost:5432/${databaseName}`
+const db = new Sequelize(
+  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  {
+    logging: true,
+  }
+);
+console.log(db);
 
 module.exports = db;
