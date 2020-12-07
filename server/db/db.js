5e0c40324727bb1c6d140b1c16b8dfd186e2003b
esy <yenigun13@gmail.com>
2020-12-06 19:38:52 -0500

    DB: Local shift to prod --remote url

--- server/db/db.js
+++ server/db/db.js
@@ -4,22 +4,17 @@ const dbConfig = require("./db.config");
 const databaseName =
   "stacktrack" + process.env.NODE_ENV === "test" ? "test" : "";
 
-const db = new Sequelize(
-  dbConfig.db || `postgres://localhost:5432/${databaseName}`,
-  dbConfig.USER,
-  dbConfig.PASSWORD,
-  {
-    host: dbConfig.HOST,
-    logging: true,
-    dialect: dbConfig.dialect,
-    pool: {
-      max: dbConfig.pool.max,
-      min: dbConfig.pool.min,
-      acquire: dbConfig.pool.acquire,
-      idle: dbConfig.pool.idle,
-    },
-  }
-);
-// console.log(db);
+const db = new Sequelize(dbConfig.db, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  logging: true,
+  dialect: dbConfig.dialect,
+  pool: {
+    max: dbConfig.pool.max,
+    min: dbConfig.pool.min,
+    acquire: dbConfig.pool.acquire,
+    idle: dbConfig.pool.idle,
+  },
+});
+// console.log(db); || `postgres://localhost:5432/${databaseName}`
 
 module.exports = db;
