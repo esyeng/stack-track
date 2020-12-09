34630e6a4ae58d9e02029fd5c299ee9f0c3d039e
esy <yenigun13@gmail.com>
2020-12-09 17:24:37 -0500

    webpack config tweaks

--- server/index.js
+++ server/index.js
@@ -55,12 +55,12 @@ app.use(passport.session());
 app.use("/auth", require("./auth"));
 app.use("/api", require("./api"));
 
-app.use("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "public/index.html"));
-});
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
-});
+// app.use("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "..", "public/index.html"));
+// });
+// app.get("/*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../public/index.html"));
+// });
 
 app.use((req, res, next) =>
   path.extname(req.path).length > 0 ? res.status(404).send("Not found") : next()
