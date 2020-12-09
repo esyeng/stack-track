ddd597bd04a3bb258dc9927fba6852f11147eb20
esy <yenigun13@gmail.com>
2020-12-09 17:03:08 -0500

    webpack config tweaks

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "public"),
     filename: "bundle.js",
-    publicPath: "/public",
+    publicPath: "/",
   },
   resolve: {
     extensions: [".js", ".jsx"],
