884136578ea83d11610784d50cc62bbb466fc01b
esy <yenigun13@gmail.com>
2020-12-10 10:32:39 -0500

    webpack config tweaks

--- server/index.js
+++ server/index.js
@@ -54,7 +54,7 @@ app.use(passport.session());
 app.use("/auth", require("./auth"));
 app.use("/api", require("./api"));
 
-app.use(express.static(path.join(__dirname, "..", "public")));
+app.use(express.static(path.resolve(__dirname, "..", "public")));
 
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "/public/index.html"));
