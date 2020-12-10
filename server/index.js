06a3928ded26336bab71ec4009aa89482be5112f
esy <yenigun13@gmail.com>
2020-12-10 10:47:46 -0500

    webpack config tweaks

--- server/index.js
+++ server/index.js
@@ -57,7 +57,7 @@ app.use("/api", require("./api"));
 app.use(express.static(path.resolve(__dirname, "..", "public")));
 
 app.use("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "public/index.html"));
+  res.sendFile(path.join(__dirname, "..", "public/bundle.js"));
 });
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "/public/index.html"));
